a26586ca4be7e2e10848c40db42a4b63febe6399
alextrm <tremeyalex@gmail.com>
2024-03-30 18:09:14 +0100

    third  commit

--- components/project.tsx
+++ components/project.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useRef } from "react";
-import { projectsData } from "@/lib/data";
 import { motion, useScroll, useTransform } from "framer-motion";
 
 type ProjectProps = {
@@ -35,7 +34,7 @@ export default function Project({
       className="group mb-3 sm:mb-8 last:mb-0"
     >
       <section
-        className={` bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative hover:bg-gray-200 transition dark:text-white dark:bg-white/10 dark:hover:bg-white/20`}
+        className={`bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative hover:bg-gray-200 transition dark:text-white dark:bg-white/10 dark:hover:bg-white/20`}
       >
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[100%] flex flex-col h-full">
           <h3 className="text-2xl font-semibold">{title}</h3>
