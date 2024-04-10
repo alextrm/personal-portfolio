62512f99f98a2b6b8ddc82c36f608e22557626bd
alextrm <tremeyalex@gmail.com>
2024-04-10 19:49:16 +0200

    fifth commit

--- components/header.tsx
+++ components/header.tsx
@@ -32,9 +32,9 @@ export default function Header() {
               >
                 <Link
                   className={clsx(
-                    "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+                    "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition",
                     {
-                      "text-gray-950 dark:text-gray-200":
+                      "text-gray-950":
                         activeSection === link.name,
                     }
                   )}
@@ -48,7 +48,7 @@ export default function Header() {
   
                   {link.name === activeSection && (
                     <motion.span
-                      className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
+                      className="bg-gray-100 rounded-full absolute inset-0 -z-10"
                       layoutId="activeSection"
                       transition={{
                         type: "spring",
